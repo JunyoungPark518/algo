6ce15d5401835d1a831b7f1776cc8c26473c93f2
jyp90 <babungv@gmail.com>
2022-03-05 23:31:20 +0900

    study: 알고리즘 풀이

---
+++ src/study/week1/Problem1931.java
@@ -0,0 +1,115 @@
+package study.week1;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 한 개의 회의실이 있는데 이를 사용하고자 하는 N개의 회의에 대하여 회의실 사용표를 만들려고 한다.
+ * 각 회의 I에 대해 시작시간과 끝나는 시간이 주어져 있고, 각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 회의의 최대 개수를 찾아보자.
+ * 단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다.
+ * 회의의 시작시간과 끝나는 시간이 같을 수도 있다. 이 경우에는 시작하자마자 끝나는 것으로 생각하면 된다.
+ */
+public class Problem1931 {
+
+    private int conferenceCount;
+
+    private List<Conference> conferences;
+
+    public Problem1931() {
+        this.conferenceCount = 11;
+        this.conferences = List.of(
+            Conference.of(1, 4),
+            Conference.of(3, 5),
+            Conference.of(0, 6),
+            Conference.of(5, 7),
+            Conference.of(3, 8),
+            Conference.of(5, 9),
+            Conference.of(6, 10),
+            Conference.of(8, 11),
+            Conference.of(8, 12),
+            Conference.of(2, 13),
+            Conference.of(12, 14)
+        );
+    }
+
+    public Conference getMinStart() {
+        Comparator<Conference> comparator = Comparator.comparingInt(Conference::getStart);
+        return conferences.stream().min(comparator).get();
+    }
+
+    public Conference getMinDiff() {
+        Conference result = new Conference(0, 24);
+        int diff = 24;
+        for(Conference conference : conferences) {
+            if(conference.getEnd() - conference.getStart() < diff) {
+                result = conference;
+                diff = conference.getEnd() - conference.getStart();
+            }
+        }
+        return result;
+    }
+
+    public boolean isNext(Conference arg1, Conference arg2) {
+        return arg1.getEnd() < arg2.getStart();
+    }
+
+    public int solution() {
+        int result = 1;
+        int saved = 0;
+        for(int index = 0; index < conferences.size(); index++) {
+            ArrayList<Conference> pop = new ArrayList<>(conferences);
+            Conference basement = conferences.get(index);
+            System.out.println("basement = " + basement);
+            result = 1;
+            for(int recursive = 0; recursive < pop.size(); recursive++) {
+                final Conference compareObject = pop.get(recursive);
+                if(isNext(basement, compareObject)) {
+                    result++;
+                    basement = compareObject;
+                    System.out.println("diff = " + compareObject);
+                    pop.remove(recursive);
+                    recursive = 0;
+                }
+            }
+            if(result > saved) {
+                saved = result;
+            }
+        }
+
+        System.out.println("saved = " + saved);
+        return saved;
+    }
+
+    public static void main(String[] args) {
+        final Problem1931 problem1931 = new Problem1931();
+        problem1931.solution();
+    }
+}
+
+class Conference {
+
+    private Integer start;
+    private Integer end;
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public Integer getEnd() {
+        return end;
+    }
+
+    public Conference(Integer start, Integer end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public static Conference of(Integer start, Integer end) {
+        return new Conference(start, end);
+    }
+
+    public String toString() {
+        return "Start: " + this.start + " / End: " + this.end;
+    }
+}
---
+++ src/study/week1/Problem2606.java
@@ -0,0 +1,110 @@
+package study.week1;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 신종 바이러스인 웜 바이러스는 네트워크를 통해 전파된다.
+ * 한 컴퓨터가 웜 바이러스에 걸리면 그 컴퓨터와 네트워크 상에서 연결되어 있는 모든 컴퓨터는 웜 바이러스에 걸리게 된다.
+ *
+ * 예를 들어 7대의 컴퓨터가 <그림 1>과 같이 네트워크 상에서 연결되어 있다고 하자.
+ * 1번 컴퓨터가 웜 바이러스에 걸리면 웜 바이러스는 2번과 5번 컴퓨터를 거쳐 3번과 6번 컴퓨터까지 전파되어 2, 3, 5, 6 네 대의 컴퓨터는
+ * 웜 바이러스에 걸리게 된다. 하지만 4번과 7번 컴퓨터는 1번 컴퓨터와 네트워크상에서 연결되어 있지 않기 때문에 영향을 받지 않는다.
+ *
+ * 어느 날 1번 컴퓨터가 웜 바이러스에 걸렸다.
+ * 컴퓨터의 수와 네트워크 상에서 서로 연결되어 있는 정보가 주어질 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 출력하는 프로그램을 작성하시오.
+ */
+public class Problem2606 {
+
+    private List<Link> computers;
+    private int computerCounts;
+
+    public Problem2606() {
+        this.computerCounts = 7;
+        this.computers = List.of(
+            Link.of(1, 2),
+            Link.of(2, 3),
+            Link.of(1, 5),
+            Link.of(5, 2),
+            Link.of(5, 6),
+            Link.of(4, 7)
+        );
+    }
+
+    public boolean isConnected(Link first, Link second) {
+        if(first.getFirst() == second.getSecond()) {
+            return true;
+        }
+        if(second.getFirst() == first.getSecond()) {
+            return true;
+        }
+        return false;
+    }
+
+    public int solution(int virus) {
+        int result = -1;
+        Set<Integer> defectedList = new HashSet<>();
+        final List<Link> spreadList = has(virus);
+        for(int index = 0; index < computerCounts-2; index++) {
+            if(isConnected(computers.get(index), computers.get(index+1))) {
+                defectedList.add(computers.get(index).getFirst());
+                defectedList.add(computers.get(index).getSecond());
+                defectedList.add(computers.get(index+1).getFirst());
+                defectedList.add(computers.get(index+1).getSecond());
+            }
+        }
+        result += defectedList.size();
+        return result;
+    }
+
+    public List<Link> has(int virus) {
+        return computers.stream()
+            .filter(link -> !link.isChecked())
+            .filter(link -> link.getFirst() == virus || link.getSecond() == virus)
+            .peek(Link::setDefected)
+            .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        final Problem2606 problem2606 = new Problem2606();
+        problem2606.solution(1);
+    }
+}
+
+class Link {
+    private int first;
+    private int second;
+    private boolean checked;
+
+    public Link(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static Link of(int first, int second) {
+        return new Link(first, second);
+    }
+
+    public int getFirst() {
+        return this.first;
+    }
+
+    public int getSecond() {
+        return this.second;
+    }
+
+    public void setDefected() {
+        this.checked = true;
+    }
+
+    public boolean isChecked() {
+        return this.checked;
+    }
+
+    public String toString() {
+        return "First : " + this.first +
+            " / Second : " + this.second;
+    }
+}
